6aa336859390cf5ff0a163ead739f8dd949b378d
Gary Huang <garyhuaang@gmail.com>
2022-03-15 19:23:03 -0700

    Basic Setup w/ leaflet API
    Linked leaflet API and reorganized files.

---
+++ script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+    function (pos) {
+        const { latitude } = pos.coords;
+        const { longitude } = pos.coords;
+        const coords = [latitude, longitude]
+
+        const map = L.map('map').setView(coords, 12)
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+
+
+        map.on('click', function (mapEvent) {
+            console.log(mapEvent)
+            const { lat, lng } = mapEvent.latlng;
+            L.marker([lat, lng])
+                .addTo(map)
+                .bindPopup(L.popup({
+                    maxWidth: 250,
+                    minWidth: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: 'running-popup'
+                }))
+                .setPopupContent('Workout')
+                .openPopup();
+        });
+    },
+    function () {
+        alert('Could not get your position');
+    }
+)
+
